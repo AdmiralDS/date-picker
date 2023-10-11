679572cb39593d39d7497bed8f1d4a9a6c9901b5
Larisa Markova <lovemirror@rambler.ru>
2023-10-11 13:53:18 +0300

    Add new Calendar

---
+++ src/stories/calendarStories/Calendar.stories.tsx
@@ -0,0 +1,17 @@
+/*import type { Meta } from '@storybook/react';
+
+export default {
+  title: 'Admiral-2.1/Calendar',
+  component: Calendar,
+  parameters: {
+    docs: {
+      source: {
+        code: null,
+      },
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=39%3A53407',
+    },
+  },
+} as Meta<typeof Calendar>;*/
