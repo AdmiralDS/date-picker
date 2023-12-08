ceffe2c9063c23b61083af01194e8c4ba3f2d509
Larisa Markova <lovemirror@rambler.ru>
2023-12-08 15:33:38 +0300

    Remove redundant component

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ export * from './components/YearRangeDoublePickerCalendar';
 export * from './components/CalendarOld';
 export * from './components/DateCalendar';
 export * from './components/DateRangeCalendar';
-export * from './components/DateRangeCalendarDouble';
 export * from './components/MonthCalendar';
 export * from './components/MonthRangeCalendar';
 export * from './components/YearCalendar';
