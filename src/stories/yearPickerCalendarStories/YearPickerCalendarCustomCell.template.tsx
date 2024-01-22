7be55dd9b98fd91aa84a2c741ea934ec3da45fad
Larisa Markova <lovemirror@rambler.ru>
2024-01-22 16:19:38 +0300

    Fix custom year cell template

--- src/stories/yearPickerCalendarStories/YearPickerCalendarCustomCell.template.tsx
+++ src/stories/yearPickerCalendarStories/YearPickerCalendarCustomCell.template.tsx
@@ -1,53 +1,75 @@
 import type { MouseEventHandler } from 'react';
-//import { useState } from 'react';
+import { memo } from 'react';
 
 import type { YearPickerCalendarProps } from '@admiral-ds/date-picker';
 import { YearPickerCalendar } from '#src/components/YearPickerCalendar';
-//import type { Dayjs } from 'dayjs';
-import { getCurrentDate, getSelectedDate, yearIsDisabled } from '#src/components/utils.ts';
-import { DefaultYearCell } from '#src/components/DefaultCell';
-import type { RenderFunctionProps } from '#src/components/calendarInterfaces.ts';
+import type { DefaultCellProps } from '#src/components/DefaultCell';
+import { DefaultCell } from '#src/components/DefaultCell';
+import { YEAR_CELL_HEIGHT, YEAR_CELL_WIDTH } from '#src/components/DefaultCell/constants.ts';
+import {
+  baseYearCellMixin,
+  currentYearCellMixin,
+  disabledYearCellMixin,
+  hiddenYearCellMixin,
+  selectedYearCellMixin,
+} from '#src/components/DefaultCell/mixins.tsx';
+import { css } from 'styled-components';
+
+const getDefaultYearCellMixin = (
+  selected?: boolean,
+  disabled?: boolean,
+  hidden?: boolean,
+  isCurrent?: boolean,
+  isActive?: boolean,
+) => {
+  if (hidden) return hiddenYearCellMixin;
+  if (disabled) return disabledYearCellMixin;
+  if (selected) return selectedYearCellMixin;
+  if (isActive) return baseYearCellMixin;
+  if (isCurrent) return currentYearCellMixin;
+  return baseYearCellMixin;
+};
+
+const getYearCellDataAttributes = (isCurrent?: boolean, isActive?: boolean): Record<string, any> => {
+  return {
+    'data-is-current-year': isCurrent ? isCurrent : undefined,
+    'data-is-active-year': isActive ? isActive : undefined,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const DefaultYearCell = ({ isCurrent, isHoliday, ...props }: DefaultCellProps) => {
+  const yearCellMixin = getDefaultYearCellMixin(
+    props.selected,
+    props.disabled,
+    props.hidden,
+    isCurrent,
+    props.isActive,
+  );
+  const cellMixin = css`
+    ${yearCellMixin};
+    color: red;
+  `;
+  return (
+    <DefaultCell
+      width={YEAR_CELL_WIDTH}
+      height={YEAR_CELL_HEIGHT}
+      cellMixin={cellMixin}
+      data-cell-type="yearCell"
+      {...getYearCellDataAttributes(!!isCurrent, props.isActive)}
+      {...props}
+    />
+  );
+};
+const MemoCell = memo(DefaultYearCell);
 
 export const YearPickerCalendarCustomCellTemplate = ({ locale, ...props }: YearPickerCalendarProps) => {
   const localeInner = locale || 'ru';
-  //const [activeDateState, setActiveDateState] = useState<Dayjs | undefined>();
-  //const [selectedDateState, setSelectedDateState] = useState<Dayjs | undefined>();
-
-  const getYearCellDataAttributes = (value?: string, isCurrent?: boolean, isActive?: boolean): Record<string, any> => {
-    return {
-      'data-value': value ? value : undefined,
-      'data-is-current-year': isCurrent ? isCurrent : undefined,
-      'data-is-active-year': isActive ? isActive : undefined,
-    };
-  };
-
-  const renderDefaultYearCell = ({ date, selected, active, onCellMouseEnter, onCellClick }: RenderFunctionProps) => {
-    const selectedDate = getSelectedDate(selected);
-    const disabled = yearIsDisabled(date);
-    const isCurrent = date.isSame(getCurrentDate(locale), 'year');
-    const isActive = date.isSame(active, 'year');
-    console.log(`render custom cell`);
-    return (
-      <DefaultYearCell
-        key={date.toString()}
-        cellContent={date.year()}
-        disabled={disabled}
-        selected={date.isSame(selectedDate, 'year')}
-        isCurrent={isCurrent}
-        isActive={isActive}
-        onMouseEnter={() => onCellMouseEnter?.(date, disabled)}
-        onClick={() => onCellClick?.(date, disabled)}
-        {...getYearCellDataAttributes(date.toString(), isCurrent, isActive)}
-      />
-    );
-  };
 
   const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
     const clickedCell = (e.target as HTMLDivElement).dataset.value;
     console.log(`click on ${clickedCell}`);
   };
 
-  return (
-    <YearPickerCalendar {...props} renderCell={renderDefaultYearCell} onClick={handleClick} locale={localeInner} />
-  );
+  return <YearPickerCalendar {...props} cell={MemoCell} onClick={handleClick} locale={localeInner} />;
 };
