24bf6742840c8f8579f4aed52eac2858435460f7
Larisa Markova <lovemirror@rambler.ru>
2024-02-05 23:05:02 +0300

    Add description

--- src/stories/dateRangePickerCalendarStories/DateRangePickerCalendarSimple.template.tsx
+++ src/stories/dateRangePickerCalendarStories/DateRangePickerCalendarSimple.template.tsx
@@ -1,7 +1,9 @@
 import type { MouseEventHandler } from 'react';
 
+import { T } from '@admiral-ds/react-ui';
 import type { DateRangePickerCalendarProps } from '@admiral-ds/date-picker';
 import { DateRangePickerCalendar } from '@admiral-ds/date-picker';
+import { WrapperHorizontal } from '#src/stories/common.tsx';
 
 export const DateRangePickerCalendarSimpleTemplate = (props: DateRangePickerCalendarProps) => {
   const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
@@ -9,5 +11,20 @@ export const DateRangePickerCalendarSimpleTemplate = (props: DateRangePickerCale
     console.log(`click on ${clickedCell}`);
   };
 
-  return <DateRangePickerCalendar {...props} onClick={handleClick} />;
+  return (
+    <WrapperHorizontal>
+      <DateRangePickerCalendar {...props} onClick={handleClick} />
+      <T font="Body/Body 2 Long" as="div">
+        Выбор диапазона дат.
+        <br />
+        При первом клике на число оно выделяется синим. Далее ведем курсор ко второму числу, числа автоматически
+        закрашиваются во всем диапазоне до даты под курсором.
+        <br />
+        При клике на второе число оно также выделяется синим. Диапазон дат выбран.
+        <br />
+        Клик на месяц/год открывает экран выбора месяца/года. Вернуться на экран выбора даты можно выбрав месяц/год,
+        либо нажав на месяц/год в шапке календаря
+      </T>
+    </WrapperHorizontal>
+  );
 };
--- src/stories/yearPickerCalendarStories/YearPickerCalendarSimple.template.tsx
+++ src/stories/yearPickerCalendarStories/YearPickerCalendarSimple.template.tsx
@@ -1,7 +1,9 @@
 import type { MouseEventHandler } from 'react';
 
+import { T } from '@admiral-ds/react-ui';
 import type { YearPickerCalendarProps } from '@admiral-ds/date-picker';
 import { YearPickerCalendar } from '#src/components/YearPickerCalendar';
+import { WrapperHorizontal } from '#src/stories/common.tsx';
 
 export const YearPickerCalendarSimpleTemplate = (props: YearPickerCalendarProps) => {
   const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
@@ -9,5 +11,15 @@ export const YearPickerCalendarSimpleTemplate = (props: YearPickerCalendarProps)
     console.log(`click on ${clickedCell}`);
   };
 
-  return <YearPickerCalendar {...props} onClick={handleClick} />;
+  return (
+    <WrapperHorizontal>
+      <YearPickerCalendar {...props} onClick={handleClick} />
+      <T font="Body/Body 2 Long" as="div">
+        Выбор года.
+        <br />
+        Цифры в шапке календаря не активны и отображают текущий временной интервал. Стрелки влево-вправо позволяют
+        менять отображаемый интервал на 20 лет назад/вперед.
+      </T>
+    </WrapperHorizontal>
+  );
 };
