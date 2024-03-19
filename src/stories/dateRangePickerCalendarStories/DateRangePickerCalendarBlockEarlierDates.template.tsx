0be9cc0502f7826c34757b90710556d18b612007
Larisa Markova <lovemirror@rambler.ru>
2024-03-19 18:32:26 +0300

    Add blocking prev arrow

--- src/stories/dateRangePickerCalendarStories/DateRangePickerCalendarBlockEarlierDates.template.tsx
+++ src/stories/dateRangePickerCalendarStories/DateRangePickerCalendarBlockEarlierDates.template.tsx
@@ -1,15 +1,62 @@
 import type { MouseEventHandler } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+import minMax from 'dayjs/plugin/minMax';
 
 import { T } from '@admiral-ds/react-ui';
-import type { DateRangePickerCalendarProps } from '@admiral-ds/date-picker';
+import type { CalendarViewMode, DateRangePickerCalendarProps } from '@admiral-ds/date-picker';
 import { DateRangePickerCalendar } from '@admiral-ds/date-picker';
 
 import { WrapperHorizontal, WrapperVertical } from '#src/stories/common.tsx';
 import { dateIsInRange, dateIsSelected } from '#src/components/utils.ts';
+import { YEARS_ON_SCREEN } from '#src/components/YearsOfTwentyYearsWidget/constants.ts';
+
+dayjs.extend(minMax);
+
+const getUnitType = (mode: CalendarViewMode) => {
+  switch (mode) {
+    case 'years':
+      return 'year';
+    case 'months':
+      return 'month';
+    case 'dates':
+    default:
+      return 'date';
+  }
+};
+const getPrevDate = (date: Dayjs, viewMode: CalendarViewMode) => {
+  switch (viewMode) {
+    case 'years':
+      return date.subtract(YEARS_ON_SCREEN, 'year');
+    case 'months':
+      return date.subtract(1, 'year');
+    case 'dates':
+    default:
+      return date.subtract(1, 'month');
+  }
+};
+
+const getMinDate = (date1?: Dayjs, date2?: Dayjs, date3?: Dayjs) => {
+  if (!date1 && !date2 && !date3) return undefined;
+  const dates: Dayjs[] = [];
+  if (date1) dates.push(date1);
+  if (date2) dates.push(date2);
+  if (date3) dates.push(date3);
+  return dayjs.min(dates);
+};
 
 export const DateRangePickerCalendarBlockEarlierDatesTemplate = (props: DateRangePickerCalendarProps) => {
+  const [viewMode, setViewMode] = useState<CalendarViewMode>('dates');
+  const handleViewModeChange = (mode: CalendarViewMode) => {
+    setViewMode(mode);
+  };
+
+  const [viewDate, setViewDate] = useState<Dayjs>(dayjs());
+  const handleViewDateChange = (date: Dayjs) => {
+    setViewDate(date);
+  };
+
   const [activeDateRangeEnd, setActiveDateRangeEnd] = useState<Dayjs | undefined>();
   const handleActiveDateRangeEndChange = (date?: Dayjs) => {
     setActiveDateRangeEnd(date);
@@ -19,6 +66,20 @@ export const DateRangePickerCalendarBlockEarlierDatesTemplate = (props: DateRang
   const handleSelectedDateRangeChange = (dateRange: [Dayjs | undefined, Dayjs | undefined]) => {
     setSelectedDateRange(dateRange);
   };
+
+  const [prevArrowDisabled, setPrevArrowDisabled] = useState<boolean>(false);
+  useEffect(() => {
+    const prevDate = getPrevDate(viewDate, viewMode);
+    const unit = getUnitType(viewMode);
+    const minDate = getMinDate(activeDateRangeEnd, selectedDateRange?.[0], selectedDateRange?.[1]);
+    if (minDate && prevDate.isBefore(minDate, unit)) {
+      setPrevArrowDisabled(true);
+    } else {
+      if (prevArrowDisabled) {
+        setPrevArrowDisabled(false);
+      }
+    }
+  }, [viewDate, viewMode, activeDateRangeEnd]);
 
   const dateAttrs = (date: Dayjs) => {
     if (activeDateRangeEnd && date.isBefore(activeDateRangeEnd, 'date')) {
@@ -42,6 +103,11 @@ export const DateRangePickerCalendarBlockEarlierDatesTemplate = (props: DateRang
         onActiveDateRangeEndValueChange={handleActiveDateRangeEndChange}
         dateAttributes={dateAttrs}
         onSelectedDateRangeValueChange={handleSelectedDateRangeChange}
+        defaultViewModeValue={'dates'}
+        defaultDateValue={dayjs()}
+        onDateValueChange={handleViewDateChange}
+        onViewModeChange={handleViewModeChange}
+        prevButtonProps={{ disabled: prevArrowDisabled }}
       />
       <WrapperVertical>
         <T font="Subtitle/Subtitle 2" as="div">
