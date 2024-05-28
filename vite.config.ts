9fee1f32810da7a6cb3f5ea3620bd1a18683663d
Andrey Litvinov <drune.email@gmail.com>
2024-05-28 19:49:57 +0300

    add vitest env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import dts from 'vite-plugin-dts';
 import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['./.test/setup.ts'],
+    globals: true,
+  },
   plugins: [
     react({ plugins: [['@swc/plugin-styled-components', {}]] }),
     svgr({
@@ -16,6 +23,7 @@ export default defineConfig({
         },
       },
     }),
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.build.json' }),
   ],
   build: {
     copyPublicDir: false,
@@ -24,25 +32,25 @@ export default defineConfig({
     // https://vitejs.dev/guide/build.html#library-mode
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
       formats: ['es'],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: [...Object.keys(pkg.peerDependencies || {}), /dayjs/, 'react/jsx-runtime'],
-      input: ['./src/index.ts'],
-      output: [
-        {
-          dir: 'dist',
-          entryFileNames: '[name].js',
-          format: 'es',
-          preserveModules: true,
-          preserveModulesRoot: 'src',
-          sourcemap: true,
-          interop: 'auto',
-          assetFileNames: 'assets/[name][extname]',
-        },
-      ],
+      external: [...Object.keys(pkg.peerDependencies || {}).map((dep) => new RegExp(dep))],
+      // input: ['./src/index.ts'],
+      // output: [
+      //   {
+      //     dir: 'dist',
+      //     entryFileNames: '[name].js',
+      //     format: 'es',
+      //     preserveModules: true,
+      //     preserveModulesRoot: 'src',
+      //     interop: 'auto',
+      //     assetFileNames: 'assets/[name][extname]',
+      //   },
+      // ],
     },
   },
 });
