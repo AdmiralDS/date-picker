99a83e2539a7b8e9970aa7355ceb0f4320abee8e
Larisa Markova <lovemirror@rambler.ru>
2025-07-17 16:27:56 +0300

    Add MonthRangePicker

--- lib/main.ts
+++ lib/main.ts
@@ -25,6 +25,7 @@ export * from './components/DatePicker';
 export * from './components/Input';
 export * from './components/InputIconButton';
 export * from './components/DateRangePicker';
+export * from './components/MonthRangePicker';
 export * from './components/Calendar';
 export * from './dateFormatters';
 export * from './dateTimestampUtils';
---
+++ src/stories/monthRangePickerStories/MonthRangePickerSimple.template.tsx
@@ -0,0 +1,67 @@
+import { T } from '@admiral-ds/react-ui';
+import { MonthRangePicker } from '@admiral-ds/date-picker';
+import { maskitoDateOptionsGenerator } from '@maskito/kit';
+import { useMaskito } from '@maskito/react';
+
+import { WrapperHorizontal, WrapperVertical } from '#src/stories/common.tsx';
+import { useState } from 'react';
+
+const defaultInputStartProps = {
+  placeholder: 'мм.гггг',
+  dataPlaceholder: 'мм.гггг',
+  value: '',
+};
+const defaultInputEndProps = {
+  placeholder: 'мм.гггг',
+  dataPlaceholder: 'мм.гггг',
+  value: '',
+};
+
+export const MonthRangePickerSimpleTemplate = () => {
+  const [inputStartValue, setInputStartValue] = useState(defaultInputStartProps.value);
+  const [inputEndValue, setInputEndValue] = useState(defaultInputEndProps.value);
+  const dateOptions = maskitoDateOptionsGenerator({ mode: 'mm/yyyy' });
+  const maskedDateInputStartRef = useMaskito({ options: dateOptions });
+  const maskedDateInputEndRef = useMaskito({ options: dateOptions });
+
+  return (
+    <WrapperHorizontal>
+      <WrapperVertical>
+        <MonthRangePicker
+          inputPropsStart={{
+            ...defaultInputStartProps,
+            value: inputStartValue,
+            onInput: (e) => setInputStartValue(e.currentTarget.value),
+            ref: maskedDateInputStartRef,
+          }}
+          inputPropsEnd={{
+            ...defaultInputEndProps,
+            value: inputEndValue,
+            onInput: (e) => setInputEndValue(e.currentTarget.value),
+            ref: maskedDateInputEndRef,
+          }}
+        />
+      </WrapperVertical>
+
+      <WrapperVertical>
+        <T font="Subtitle/Subtitle 2" as="div">
+          Выбор даты
+        </T>
+        <T font="Body/Body 2 Long" as="div">
+          Высота календаря постоянна и состоит из шести рядов чисел. Текущий месяц начинается с первого ряда и, если его
+          даты не заходят на шестой ряд, то там ставятся пустые ячейки (Empty).
+          <br />
+          <br />
+          Текущая дата выделяется обводкой в виде черного кружка. Ховер на дате подсвечивается обводкой синего цвета.
+          Выбранная дата отмечается синим кружком.
+          <br />
+          При ховере на месяце, годе, стрелках вправо/влево появляются тултипы с подсказками.
+          <br />
+          <br />
+          Клик на месяц/год открывает экран выбора месяца/года. Вернуться на экран выбора даты можно выбрав месяц/год,
+          либо нажав на месяц/год в шапке календаря
+        </T>
+      </WrapperVertical>
+    </WrapperHorizontal>
+  );
+};
